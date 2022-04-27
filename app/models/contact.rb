33edbd01e53f68f858addcd515f5fb3fbba9b5a6
Mathias Nikkel <mathiasnikkel@gmail.com>
2022-04-27 14:03:06 -0300

    new address model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
+  has_one :address
 
   accepts_nested_attributes_for :phones, allow_destroy: true
+  accepts_nested_attributes_for :address, allow_destroy: true
 
   def valid
     true
@@ -11,7 +13,7 @@ class Contact < ApplicationRecord
   # redefines as_json method to include valid method
   def as_json(options = {})
     h = super(options.merge(
-      include: [:phones, { kind: { only: %i[description id] } }]
+      include: [:phones, :address,{ kind: { only: %i[description id] } }]
     ))
 
     h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
