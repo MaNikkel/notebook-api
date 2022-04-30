7a0a45049d6f7a19ca47cf3a38b5a7b61cf7b95d
Mathias Nikkel <mathiasnikkel@gmail.com>
2022-04-30 11:28:24 -0300

    refactoring for using ams

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,18 +6,4 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
 
-  def valid
-    true
-  end
-
-  # redefines as_json method to include valid method
-  def as_json(options = {})
-    h = super(options.merge(
-      include: [:phones, :address,{ kind: { only: %i[description id] } }]
-    ))
-
-    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
-
-    h
-  end
 end
